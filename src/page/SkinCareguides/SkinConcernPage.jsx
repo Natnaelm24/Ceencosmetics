b41aca5ffa79540d18391a844553a48aa6f90826
Natnaelm24 <natnael.mola.4@gmail.com>
2025-12-04 14:54:46 +0300

    skinconcern fix

--- src/page/SkinCareguides/SkinConcernPage.jsx
+++ src/page/SkinCareguides/SkinConcernPage.jsx
@@ -1,21 +1,63 @@
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import Dryness from "./Dryness/Dryness";
+// import SkinAging from "./SkinAging/SkinAging";
+// import Dullness from "./Dullness/Dullness";
+// import UnevenSkinTone from "./Unevenskintone/Unevenskintone";
+// import UVProtection from "./Uvprotection/Uvprotection";
+
+// function SkinConcernPage() {
+//   const { slug } = useParams();
+//   console.log("Slug:", slug);
+
+//   const componentMap = {
+//     "dryness-and-dehydration": Dryness,
+//     "skin-aging": SkinAging,
+//     "skin-dullness": Dullness,
+//     "uneven-skin-tone": UnevenSkinTone,
+//     "uv-protection": UVProtection,
+//   };
+
+//   const Component = componentMap[slug];
+
+//   if (!Component) {
+//     return (
+//       <div className="text-center py-20">
+//         <p className="text-xl text-gray-600">Skin concern not found</p>
+//       </div>
+//     );
+//   }
+
+//   return <Component />;
+// }
+
+// export default SkinConcernPage;
+
+
+
+
+
 import React from "react";
 import { useParams } from "react-router-dom";
+
+// Import components based on your actual folder/file names
 import Dryness from "./Dryness/Dryness";
 import SkinAging from "./SkinAging/SkinAging";
 import Dullness from "./Dullness/Dullness";
-import UnevenSkinTone from "./Unevenskintone/Unevenskintone";
-import UVProtection from "./Uvprotection/Uvprotection";
+import Unevenskintone from "./Unevenskintone/Unevenskintone";
+import Uvprotection from "./Uvprotection/Uvprotection";
 
 function SkinConcernPage() {
   const { slug } = useParams();
   console.log("Slug:", slug);
 
+  // Map URL slugs to components
   const componentMap = {
     "dryness-and-dehydration": Dryness,
     "skin-aging": SkinAging,
     "skin-dullness": Dullness,
-    "uneven-skin-tone": UnevenSkinTone,
-    "uv-protection": UVProtection,
+    "uneven-skin-tone": Unevenskintone,
+    "uv-protection": Uvprotection,
   };
 
   const Component = componentMap[slug];
