dc5166f9275ab84d0efaaa2eb9212a7ef988c3e1
Natnaelm24 <natnael.mola.4@gmail.com>
2025-12-04 12:33:17 +0300

    fix

--- src/page/SkinCareguides/SkinConcernPage.jsx
+++ src/page/SkinCareguides/SkinConcernPage.jsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import Dryness from "./Dryness/Dryness";
-import SkinAging from "./SkinAging/SkinAging";
-import Dullness from "./Dullness/Dullness";
-import UnevenSkinTone from "./Unevenskintone/Unevenskintone";
-import UVProtection from "./Uvprotection/Uvprotection";
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import Dryness from "./Dryness/Dryness";
+// import SkinAging from "./SkinAging/SkinAging";
+// import Dullness from "./Dullness/Dullness";
+// import UnevenSkinTone from "./Unevenskintone/Unevenskintone";
+// import UVProtection from "./Uvprotection/Uvprotection";
 
-function SkinConcernPage() {
-  const { slug } = useParams();
+// function SkinConcernPage() {
+//   const { slug } = useParams();
 
-  // Map slugs to components
-  const componentMap = {
-    "dryness-and-dehydration": Dryness,
-    "skin-aging": SkinAging,
-    "skin-dullness": Dullness,
-    "uneven-skin-tone": UnevenSkinTone,
-    "uv-protection": UVProtection,
-  };
+//   // Map slugs to components
+//   const componentMap = {
+//     "dryness-and-dehydration": Dryness,
+//     "skin-aging": SkinAging,
+//     "skin-dullness": Dullness,
+//     "uneven-skin-tone": UnevenSkinTone,
+//     "uv-protection": UVProtection,
+//   };
 
-  const Component = componentMap[slug];
+//   const Component = componentMap[slug];
 
-  if (!Component) {
-    return (
-      <div className="text-center py-20">
-        <p className="text-xl text-gray-600">Skin concern not found</p>
-      </div>
-    );
-  }
+//   if (!Component) {
+//     return (
+//       <div className="text-center py-20">
+//         <p className="text-xl text-gray-600">Skin concern not found</p>
+//       </div>
+//     );
+//   }
 
-  return <Component />;
-}
+//   return <Component />;
+// }
 
-export default SkinConcernPage;
+// export default SkinConcernPage;
