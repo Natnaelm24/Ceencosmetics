16772d558a0ce26702c5f3c882fff98040f00782
Natnaelm24 <natnael.mola.4@gmail.com>
2025-12-04 16:45:06 +0300

    its d

--- src/component/Concern.jsx
+++ src/component/Concern.jsx
@@ -1,11 +1,16 @@
 // import React from "react";
 import { useParams } from "react-router-dom";
+import Dryness from "../page/SkinCareGuides/Dryness/Dryness";
+import SkinAging from "../page/SkinCareGuides/SkinAging/SkinAging";
+import Dullness from "../page/SkinCareGuides/Dullness/Dullness";
+import UnevenSkinTone from "../page/SkinCareGuides/Unevenskintone/Unevenskintone";
+import UVProtection from "../page/SkinCareGuides/Uvprotection/Uvprotection";
 
-import Dryness from "./Dryness/Dryness";
-import SkinAging from "./SkinAging/SkinAging";
-import Dullness from "./Dullness/Dullness";
-import Unevenskintone from "./Unevenskintone/Unevenskintone";
-import Uvprotection from "./Uvprotection/Uvprotection";
+// import Dryness from "./Dryness/Dryness";
+// import SkinAging from "./SkinAging/SkinAging";
+// import Dullness from "./Dullness/Dullness";
+// import Unevenskintone from "./Unevenskintone/Unevenskintone";
+// import Uvprotection from "./Uvprotection/Uvprotection";
 
 function Concern() {
   const { slug } = useParams();
@@ -16,8 +21,8 @@ function Concern() {
     "dryness-and-dehydration": Dryness, 
     "skin-aging": SkinAging,
     "skin-dullness": Dullness,
-    "uneven-skin-tone": Unevenskintone,
-    "uv-protection": Uvprotection,
+    "uneven-skin-tone": UnevenSkinTone,
+    "uv-protection": UVProtection,
   };
 
   const Component = componentMap[slug];
